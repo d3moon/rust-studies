1fe498e1a592205809ba92011349755a6a32af86
João Victor <johnnywinchest3r@gmail.com>
2025-10-02 03:23:53 -0300

    studies: references rules

--- mod3/src/main.rs
+++ mod3/src/main.rs
@@ -1,64 +1,61 @@
+/*
+ * Regras de referências em Rust
+ * Uma referência mutável por vez (Não pode haver mais de uma referência mutável ao mesmo tempo para a mesma variável)
+ * Pode  ter muitas referências imutáveis ao mesmo tempo para a mesma variável. Isso permite que várias partes do código leiam o valor ao mesmo tempo, desde que nenhuma delas tente modificá-lo.
+ * Mutável e imutável de uma varável não podem coexistir
+ * Scope of a reference (escopo de uma referência)
+ * Os dados não devem mudar quando referências imutáveis estão em escopo
+*/
+
 fn main() {
+    // 1° regra
+    let mut heap_num: Vec<i32> = vec![1, 2, 3, 4, 5];
+    let _ref1 = &mut heap_num;
+    let _ref2 = &mut heap_num; // erro: cannot borrow `heap_num` as mutable more than once at a time
+
+    // 2° regra
+    let mut heap_num2: Vec<i32> = vec![3, 4, 5];
+    let ref1 = &heap_num2; // referência imutável
+    let ref2 = &heap_num2; // outra referência imutável
+    println!("{:?} ,{:?}", ref1, ref2);
+
+    // 3° regra
+    let mut heap_num3: Vec<i32> = vec![6, 7, 8];
+    let ref3: &Vec<i32> = &heap_num3; // referência imutável
+    let ref4: &mut Vec<i32> = &mut heap_num3; // outra referência mutável -> error because it is also borrowed as immutable
+
+    //println!("{:?} ,{:?}", ref3, ref4);
+
+    // 4° regra
+    let mut heap_num4: Vec<i32> = vec![9, 10, 11];
+    let ref5: &Vec<i32> = &heap_num4; // referência imutável
+    println!("{:?}", ref5);
+
+    // Depois que a referência imutável sai de escopo (ela saiu pq ref5 saiu de escopo depois de ser usado), podemos criar uma referência mutável
+    let ref6: &mut Vec<i32> = &mut heap_num4; // referência mutável
+    ref6.push(12);
+    println!("{:?}", ref6);
+
+    // 5° regra
+    let mut heap_num5: Vec<i32> = vec![13, 14, 15];
+    let ref7: &Vec<i32> = &heap_num5; // referência imutável
+
+    /* Tu declarou heap_num5 como mutável → beleza, poderia dar heap_num5.push(99).
+       Só que tu criou ref7 como &Vec<i32> → isso é uma referência imutável.
+       Enquanto existir uma referência imutável,
+       o compilador não deixa você criar uma referência mutável (como o &mut usado por dentro do push),
+       porque poderia dar data race (alguém lendo enquanto alguém escreve).
+    */
+
     /*
-     * Nesse exemplo, veremos como alterar o valor de uma variável na pilha (stack) dentro de uma função.
-     * Passos:
-     *  - Criar uma variável mutável na pilha. Para permitir a alteração, a variável deve ser mutável.
-     *  - Criar uma função que recebe uma referência mutável para essa variável. Isso permite que a função acesse e modifique o valor original. Novo dono.
-     *  - Alterar o valor da variável dentro da função.
-     */
+        O método .push() em um Vec<T> no Rust:
+        Adiciona um novo elemento no final do vetor.
+        Recebe o valor por ownership (ou seja, o valor é movido pro vetor).
+        Se o vetor já tiver atingido a capacidade interna, ele realoca mais memória no heap (dobra ou aumenta a capacidade) e copia os elementos antigos pro novo espaço.
+        Por isso ele precisa de &mut self → está modificando a estrutura interna.
+        */
+    println!("{:?}", ref7);
 
-    // Variável na pilha (stack)-> Significa que o valor é armazenado diretamente na memória stack.
-    let mut x = 5; // Mutável
-    println!("main x: {}", x);
+    heap_num5.push(16); // ok, pq ref7 saiu de escopo
 
-    // Função que altera o valor da variável na pilha
-    fn stack_fn(number: &mut i32) {
-        // Recebe uma referência mutável, que é um ponteiro para o valor na stack
-        *number = 42; // Desreferencia o ponteiro e altera o valor na stack
-        println!("stack_fn x: {}", number); // Imprime o valor alterado
-    }
-
-    // Chama a função passando uma referência mutável para x
-    stack_fn(&mut x);
-    // O Ownership de x permanece com main, mas o valor foi alterado pela função stack_fn, nova atribuição
-    println!("main x after stack_fn: {}", x);
-
-    /*
-     * Nesse exemplo, veremos como alterar o valor de uma variável sem modificar o ownership.
-     * Passos:
-     * - Criar uma variável imutável na pilha.
-     * - Criar uma função que recebe uma cópia do valor dessa variável.
-     * - Alterar o valor dentro da função, mas isso não afetará a variável original
-     */
-    let y = 5; // Imutável
-
-    fn stack_fn2(mut x: i32) {
-        if x == 5 {
-            x = 42; // Altera só localmente
-        }
-        println!("stack_fn2 x: {}", x); // Imprime o valor local
-    }
-
-    stack_fn2(y); // Passa uma cópia do valor de y
-    println!("main y: {}", y); // y continua 5
-
-
-    // O Ownership desse exemplo é movido para o another_vec
-    // É útil quando queremos transferir a posse de um valor para outra variável
-    let some_vec = vec![1, 2, 3]; // some_vec é dono do vetor na heap
-    let another_vec = some_vec; // Ownership de some_vec é movido para another_vec, isso é possível pois tipos compostos movem ownership por padrão, enquanto tipos primitivos copiam o valor (por serem valores pequenos)
-    let yet_another_vec = &another_vec; // Referência para another_vec
-
-
-    // Podemos usar referências mutáveis para alterar valores na heap
-    // É útil quando queremos modificar o valor sem transferir o ownership
-    let mut vec_1 = vec![1, 2, 3]; // vec_1 é dono do vetor na heap
-    let mut ref1 = &mut vec_1; // ref1 é uma referência mutável para vec_1 (o valor de ref1 é um ponteiro para o valor na heap que pode ser alterado (o valor de vec_1 pode ser alterado através de ref1))
-    ref1.push(4); // Altera o valor na heap através da referência mutável
-
-    let large_string = String::from("Hello, world!"); // large_string é dono da string na heap
-    let large_string_2: String = String::from("Hello, Rust!"); // large_string_2 é dono da string na heap
-
-    // É útil quando queremos criar uma coleção de referências para valores que já possuem um dono, sem assumir a posse desses valores
-    let huge_data: Vec<&String> = vec![&large_string, &large_string_2]; // huge_data é dono do vetor que contém as strings na heap
 }
